aabed5d36fd7584b2b9e7bdc69841a585072dc65
kubesajan <kubesajan@gmail.com>
2021-04-26 22:27:11 +0200

    Update RevisionsParser.java
    Refactoring of Revisions

--- src/main/java/RevisionsParser.java
+++ src/main/java/RevisionsParser.java
@@ -4,6 +4,12 @@ public class RevisionsParser {
     private ArrayList<String> revisionVersions = new ArrayList<>();
     private ArrayList<String> revisionDates = new ArrayList<>();
     private ArrayList<String> revisionDescriptions = new ArrayList<>();
+    private String version = "";
+    private String date = "";
+    private String description = "";
+    private int currentPage = 1;
+    private int candidatePage = 0;
+
 
     public ArrayList<String> getRevisionVersions() {
         return this.revisionVersions;
@@ -19,11 +25,6 @@ public class RevisionsParser {
 
 
     public void findRevisions(ArrayList<String> lines) {
-        String version = "";
-        String date = "";
-        String description = "";
-        int currentPage = 1;
-        int candidatePage = 0;
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
             if (line.contains("\f")) {
@@ -35,37 +36,43 @@ public class RevisionsParser {
                     if (!line.isBlank()) {
                         String[] splitRevision = line.split("\\s{2,100}");
                         if (splitRevision.length == 1) {
-                            description = description.concat(splitRevision[0].trim());
+                            parseExistingRevisionDescription(splitRevision[0]);
                         } else {
                             if (!version.isBlank() && version.length() < 6) {
-                                if (!revisionVersions.contains(version)) {
-                                    revisionVersions.add(version);
-                                    revisionDates.add(date);
-                                    revisionDescriptions.add(description);
-                                }
+                                addRevision();
                             }
-
-                            description = splitRevision[splitRevision.length - 1].trim();
-                            for (int j = 0; j < splitRevision.length - 1; j++) {
-                                if (splitRevision[j].contains("."))
-                                    version = splitRevision[j].trim();
-                                else if (splitRevision[j].contains("-") || splitRevision[j].contains("/"))
-                                    date = splitRevision[j].trim();
-                            }
+                            parseNewRevision(splitRevision);
                         }
                     }
                     i++;
                     line = lines.get(i);
                 }
                 if ((revisionVersions.size() != 0 && !version.equals(revisionVersions.get(revisionVersions.size() - 1))) && version.length() < 6) {
-                    if (!revisionVersions.contains(version)) {
-                        revisionVersions.add(version);
-                        revisionDates.add(date);
-                        revisionDescriptions.add(description);
-                    }
+                    addRevision();
                 }
-                return;
             }
+        }
+    }
+
+    private void parseExistingRevisionDescription(String s) {
+        description = description.concat(s.trim());
+    }
+
+    private void parseNewRevision(String[] splitRevision) {
+        description = splitRevision[splitRevision.length - 1].trim();
+        for (int j = 0; j < splitRevision.length - 1; j++) {
+            if (splitRevision[j].contains("."))
+                version = splitRevision[j].trim();
+            else if (splitRevision[j].contains("-") || splitRevision[j].contains("/"))
+                date = splitRevision[j].trim();
+        }
+    }
+
+    private void addRevision() {
+        if (!revisionVersions.contains(version)) {
+            revisionVersions.add(version);
+            revisionDates.add(date);
+            revisionDescriptions.add(description);
         }
     }
 }
